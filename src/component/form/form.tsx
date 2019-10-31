6568e17cefcab9ad2104a8961d5e9528142a4eae
Evie Perren <evie.perren@simplyhealth.co.uk>
2019-10-31 13:20:48 +0000

    hide render if fields empty

--- src/component/form/form.tsx
+++ src/component/form/form.tsx
@@ -4,17 +4,24 @@ import Template from '../template/template';
 import FullExample from "../full-example/full-example";
 
 import CloseIcon from '../../assets/images/times-solid.svg';
+import { unwatchFile } from 'fs';
 
 const Form = () => {
     const [practiceID, setPracticeID] = useState('');
     const [text, setText] = useState('');
     const [classname, setClassname] = useState(styles.medium);
     const [popup, setPopup] = useState(false);
+    const [isRendered, showRender] = useState(false)
     
-
     function handleOnChange(eventArg: React.FormEvent<HTMLInputElement>):void{
         setPracticeID(eventArg.currentTarget.value);
+        if(practiceID === ''){
+            showRender(false);
+        } else {
+            showRender(true);
+        }
     }
+
     function handleText(eventArg: React.FormEvent<HTMLInputElement>):void{
         setText(eventArg.currentTarget.value);
     }
@@ -39,24 +46,28 @@ const Form = () => {
             uniqueClassname = 'medium';
             styling = 'border: 1px solid grey; font-size: 16px; width: 30px; font-size: 70px; background-color: #fff; border-radius: 20px; padding: 5px;'
     }
+
+    
     return(
         <Fragment>
             <div className={styles.input_container}>
-                <label>Your practice ID</label>
-                <input type="text" value={practiceID} onChange={handleOnChange} className={styles.input}/>
-                <label>Choose your text</label>
-                <input type="text" value={text} onChange={handleText} className={styles.input}/>
-                <label>Choose your styling</label>
-                <select onChange={handleSelect} placeholder="Please select">
+                <label htmlFor="practiceID" className={styles.subheading}>Your practice ID</label>
+                <input type="text" name="practiceID" value={practiceID} onChange={handleOnChange} className={styles.input}/>
+                <label htmlFor="button-text" className={styles.subheading}>Choose your text</label>
+                <input type="text" name="button-text" value={text} onChange={handleText} className={styles.input}/>
+                <label htmlFor="size" className={styles.subheading}>Choose your styling</label>
+                <select onChange={handleSelect} placeholder="Please select" id="size">
                     <option value={styles.large}>Large</option>
                     <option value={styles.medium}>Medium</option>
                     <option value={styles.small}>Small</option>
                 </select>
-                <label>Preview</label>
+                <h3 className={styles.subheading}>Preview</h3>
                 <div className={styles.example_container}>
-                    <div className={classname}>
-                        <a id={styles.example} className={classname} href='#'>{text}</a>
-                    </div>
+                    {isRendered && (
+                        <div className={classname}>
+                            <a id={styles.example} className={classname} href='#'>{text}</a>
+                        </div>
+                    )}
                 </div>
                 <button onClick={() => setPopup(true)} className={styles.button}>Get code</button>
                 <FullExample></FullExample>
@@ -70,9 +81,9 @@ const Form = () => {
                         </a>
                         <div className={styles.template_container}>
                             <h4 className={styles.template_title}>For HTML...</h4>
-                            <Template template={`<div class="${uniqueClassname}" style="${styling}"><a href="${practiceID}">${text}</a></div>`} />
+                            <Template template={`<div class="${uniqueClassname}" style="${styling}"><a href="${practiceID}" target="_blank">${text}</a></div>`} />
                             <h4 className={styles.template_title}>For HTML option 2...</h4>
-                            <Template template={`<a href="${practiceID}" class="${uniqueClassname}" style="${styling}">${text}</a>`} />
+                            <Template template={`<a href="${practiceID}" class="${uniqueClassname}" style="${styling}" target="_blank">${text}</a>`} />
                         </div>
                     </div>
                 </div>       
