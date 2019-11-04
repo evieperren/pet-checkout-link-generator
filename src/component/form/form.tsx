ca7acc021aa06ae8bfd7d3c435837d02ab68ee19
Evie Perren <evie.perren@simplyhealth.co.uk>
2019-11-04 16:23:47 +0000

    changes to form

--- src/component/form/form.tsx
+++ src/component/form/form.tsx
@@ -1,64 +1,82 @@
-import React, {useState, Fragment} from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 import styles from './form.module.scss';
 import Template from '../template/template';
-import FullExample from "../full-example/full-example";
-import paw from "../../assets/images/exotic.svg";
 import CloseIcon from '../../assets/images/times-solid.svg';
+
+export type ColourScheme = { value: "Black", displayName: "Black" } | { value: "DarkBlue", displayName: "Dark Blue" } | { value: "Maroon", displayName: "Maroon" } | { value: "Green", displayName: "Green" };
+
+export const AllColourSchemes: Array<ColourScheme> = [
+    { value: "Black", displayName: "Black"},
+    { value: "DarkBlue", displayName: "Dark Blue" },
+    { value: "Maroon", displayName: "Maroon" },
+    { value: "Green", displayName: "Green" },
+]
 
 const Form = () => {
     const [practiceID, setPracticeID] = useState('');
     const [text, setText] = useState('Click here');
-    const [classname, setClassname] = useState(styles.medium);
     const [popup, setPopup] = useState(false);
     const [isRendered, showRender] = useState(false)
+    const [returnURL, setReturnURL] = useState('');
+
+    const [colourScheme, setColourScheme] = useState<ColourScheme>({value: "Black", displayName: "Black"});
+    const [colourSetting, setColourSettings] = useState({ colour: "white", background: "black" });
+
     
-    function handleOnChange(eventArg: React.FormEvent<HTMLInputElement>):void{
+    useEffect(() => {
+        switch (colourScheme.value) {
+            case "DarkBlue":
+                // setColourSettings({ colour: styles.white, background: styles.blue });
+                setColourSettings({ colour: "white", background: "darkblue"});
+                break;
+            case "Maroon":
+                setColourSettings({ colour: "black", background: "maroon" });
+                break;
+            case "Green":
+                setColourSettings({ colour: "black", background: "green" });
+                break;
+            case "Black":
+                setColourSettings({ colour: "white", background: "black" });
+                break;
+        }
+        
+    }, [colourScheme]);
+
+    console.log(colourSetting);
+
+    const htmlContent = `<div style="font-family: Arial, Helvetica, sans-serif; background-color: ${colourSetting.background}; width: 140px; height: 45px; border-radius: 15px; display: flex; align-items: center; padding: 2px;"><a href="${practiceID}" target="_blank">${text}</a></div>`;
+
+
+    function handleOnChange(eventArg: React.FormEvent<HTMLInputElement>): void {
         setPracticeID(eventArg.currentTarget.value);
-        if(practiceID === ''){
+        if (practiceID === '') {
             showRender(false);
         } else {
             showRender(true);
         }
     }
-    
-    function handleText(eventArg: React.FormEvent<HTMLSelectElement>):void{
+    function handleReturnURL(eventArg: React.FormEvent<HTMLInputElement>): void {
+        setReturnURL(eventArg.currentTarget.value);
+    }
+    function handleText(eventArg: React.FormEvent<HTMLSelectElement>): void {
         setText(eventArg.currentTarget.value);
     }
-    function handleSelect(eventArg: React.FormEvent<HTMLSelectElement>):void{
-        setClassname(eventArg.currentTarget.value);
-    }
-    
-    let uniqueClassname = '';
-    let styleDiv = '';
-    let styleAnchor = '';
-    let styleImage = '';
-    switch(classname){
-        case 'form_large__2dtRX':
-            uniqueClassname = 'large';
-            console.log('i am large')
-            styleDiv = 'font-family: Arial, Helvetica, sans-serif; background-color: #449FE2; width: 140px; height: 45px; border-radius: 15px; display: flex; align-items: center; padding: 2px;'
-            styleAnchor = 'font-size: 18px; text-decoration: none; color: #fff;'
-            styleImage = 'width: 40px;'
-            break;
-        case 'form_small__3Ej2V':
-            uniqueClassname = 'small';
-            styleDiv = 'font-family: Arial, Helvetica, sans-serif; background-color: #449FE2; width: 120px; height: 40px; border-radius: 15px; display: flex; align-items: center; padding: 2px;'
-            styleAnchor = 'font-size: 16px; text-decoration: none; color: #fff;'
-            styleImage = 'width: 35px;'
-            break;
-        default: 
-            uniqueClassname = 'medium';
-            styleDiv = 'font-family: Arial, Helvetica, sans-serif; background-color: #449FE2; width: 100px; height: 35px; border-radius: 15px;display: flex; align-items: center; padding: 2px;'
-            styleAnchor = 'font-size: 14px; text-decoration: none; color: #fff;'
-            styleImage = 'width: 30px;'
+    function handleSelect(eventArg: React.FormEvent<HTMLSelectElement>): void {
+
+        const colourScheme = AllColourSchemes.find(cs => cs.value === eventArg.currentTarget.value);
+        if (colourScheme) {
+            setColourScheme(colourScheme);
+        }
     }
 
-    
-    return(
+    return (
         <Fragment>
             <div className={styles.input_container}>
                 <label htmlFor="practiceID" className={styles.subheading}>Your practice ID</label>
-                <input type="text" name="practiceID" value={practiceID} onChange={handleOnChange} className={styles.input}/>
+                <input type="text" name="practiceID" value={practiceID} onChange={handleOnChange} className={styles.input} />
+
+                <label htmlFor="returnURL" className={styles.subheading}>Your return URL</label>
+                <input type="text" name="returnURL" value={returnURL} onChange={handleReturnURL} className={styles.input} />
 
                 <label htmlFor="button-text" className={styles.subheading}>Choose your text</label>
                 <select onChange={handleText} placeholder="Please select" id="button-text">
@@ -68,41 +86,38 @@ const Form = () => {
                 </select>
 
                 <label htmlFor="size" className={styles.subheading}>Choose your styling</label>
-                <select onChange={handleSelect} placeholder="Please select" id="size">
-                    <option value={styles.large}>Large</option>
-                    <option value={styles.medium}>Medium</option>
-                    <option value={styles.small}>Small</option>
+                <select onChange={handleSelect} placeholder="Please select" id="size" value={colourScheme.value}>
+                    {AllColourSchemes.map((cs, index) => <option key={index} value={cs.value}>{cs.displayName}</option>)}
+                
                 </select>
-
                 <h3 className={styles.subheading}>Preview</h3>
                 <div className={styles.example_container}>
                     {isRendered && (
-                        <div className={classname}>
-                            <img src={paw} className={classname} alt="paw"/>
-                            <a id={styles.example} className={classname} href='#'>{text}</a>
-                        </div>
+                        <div dangerouslySetInnerHTML={{
+                            __html: htmlContent} } />
+                        // <div className={colourSetting.background}>
+                        //     <a className={colourSetting.colour} id={styles.example} href='#'>{text}</a>
+                            
+                        // </div>
                     )}
                 </div>
                 <button onClick={() => setPopup(true)} className={styles.button}>Get code</button>
-                <FullExample></FullExample>
             </div>
 
             {popup && (
                 <div className={styles.form_container}>
                     <div className={styles.link_container}>
                         <a onClick={() => setPopup(false)} className={styles.close}>
-                            <img className={styles.closeIcon} src={CloseIcon} alt='close popup box'/>
+                            <img className={styles.closeIcon} src={CloseIcon} alt='close popup box' />
                         </a>
                         <div className={styles.template_container}>
                             <h4 className={styles.template_title}>Copy into your website...</h4>
-                            <Template template={`<div class="${uniqueClassname}" style="${styleDiv}">
-                            <img src=${paw} style="${styleImage}" alt="paw"/>
-                            <a href="${practiceID}" style="${styleAnchor}" target="_blank">${text}</a></div>`} />
+                            <Template template={htmlContent} />
                         </div>
                     </div>
-                </div>       
+                </div>
             )}
-          
+
         </Fragment>
     )
 }
